90de8d99e175869c9980a5a1659ad7347d30c05d
Drew MacGibbon <drewmacgibbon@gmail.com>
2024-02-02 16:11:06 +0530

    chore: basic page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="p-8 w-64 mx-auto">
-    <!-- <form action="/action_page.php">
-      <input type="file" id="myFile" name="filename" />
-      <input type="submit" />
-    </form> -->
-    <AppCropper file="file" />
+  <div class="p-8 mx-auto">
+    <div class="relative bg-primary-50 w-full max-w-[1200px] h-64">
+      <AppCropper cropper-type="cover" class="absolute bottom-0 right-0" />
+
+      <div class="w-32 h-32 absolute -bottom-16 left-16 bg-red-50 rounded-full">
+        <AppCropper cropper-type="avatar" />
+      </div>
+    </div>
   </div>
 </template>
 
