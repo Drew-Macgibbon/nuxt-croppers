12928b8cb68289b13b5d1131ba19a5c399be0fdf
Drew MacGibbon <drewmacgibbon@gmail.com>
2024-02-02 11:50:57 +0530

    feat: option one working, vue advanced cropper

---
+++ components/VueAdvancedCropper.client.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="cropper-container">
+    <cropper
+      ref="cropper"
+      :src="file"
+      :stencil-size="{
+        width: 300,
+        height: 300,
+      }"
+      :stencil-props="{
+        handlers: {},
+        movable: false,
+        resizable: false,
+        aspectRatio: 1,
+      }"
+      image-restriction="stencil"
+      :default-size="defaultSize"
+      :default-position="defaultPosition"
+      :default-visible-area="defaultVisibleArea"
+      default-boundaries="fill"
+      priority="visibleArea"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Cropper } from "vue-advanced-cropper";
+import file from "~/public/files/avatar/avatar-jpg.jpg";
+import "vue-advanced-cropper/dist/style.css";
+
+const defaultPosition = {
+  left: 100,
+  top: 100,
+};
+
+const defaultSize = {
+  width: 400,
+  height: 400,
+};
+
+const defaultVisibleArea = () => ({
+  width: 800,
+  height: 775,
+  left: 63,
+  top: 668,
+});
+</script>
+
+<style scoped>
+.cropper-container {
+  /* Style your cropper container here */
+  max-width: 100%;
+  height: auto;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,15 @@
 <template>
-  <div>
-    <form action="/action_page.php">
+  <div class="p-8 w-64 mx-auto">
+    <!-- <form action="/action_page.php">
       <input type="file" id="myFile" name="filename" />
       <input type="submit" />
-    </form>
+    </form> -->
     <AppCropper file="file" />
   </div>
 </template>
 
 <script setup lang="ts">
-
 // upload button should be seperate from component, that way the cropper component is more dynamic.
-
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
