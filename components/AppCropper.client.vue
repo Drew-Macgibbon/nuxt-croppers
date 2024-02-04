54555c7d0ab825791c7e749cbdabff361ae87705
Drew MacGibbon <drewmacgibbon@gmail.com>
2024-02-04 14:03:33 +0530

    chore: remove use of composable

--- components/AppCropper.client.vue
+++ components/AppCropper.client.vue
@@ -22,20 +22,93 @@
 
 <script setup lang="ts">
 import type { CropperConfigTypes } from "~/types/cropper";
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5 MB
 
 const uploadInput = ref(null as HTMLInputElement | null);
+const errorMessage = ref<string>("");
+const image = ref<string>("");
 
-defineProps({
+const props = defineProps({
   cropperType: {
     type: String as PropType<CropperConfigTypes>,
     required: true,
   },
 });
 
+type CropperConfigDimensions = {
+  [key in CropperConfigTypes]: { minWidth: number; minHeight: number };
+};
 
-const { handleFileChange, errorMessage, image } = useCropper();
+async function checkImageDimensions(
+  imageSrc: string,
+  cropperType: CropperConfigTypes
+): Promise<boolean> {
+  const dimensions: CropperConfigDimensions = {
+    default: { minWidth: 800, minHeight: 600 },
+    avatar: { minWidth: 160, minHeight: 160 },
+    cover: { minWidth: 1300, minHeight: 400 },
+  };
 
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.onload = () => {
+      const { minWidth, minHeight } = dimensions[cropperType];
+      if (img.width >= minWidth && img.height >= minHeight) {
+        resolve(true);
+      } else {
+        errorMessage.value = `Image dimensions must be at least ${minWidth}x${minHeight}px for ${cropperType}.`;
+        resolve(false);
+      }
+    };
+    img.onerror = () => {
+      errorMessage.value = "Failed to load image for dimension check.";
+      resolve(false);
+    };
+    img.src = imageSrc;
+  });
+}
 
+function readFileAsDataURL(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = () => reject(reader.error);
+    reader.onerror = () =>
+      reject(new Error(`Failed to read the file: ${reader.error?.message}`));
+
+    reader.readAsDataURL(file);
+  });
+}
+
+function validateFileSize(fileSize: number): boolean {
+  if (fileSize > MAX_FILE_SIZE) {
+    errorMessage.value =
+      "File is too large. Please select a file smaller than 5MB.";
+    return false;
+  }
+  return true;
+}
+
+async function handleFileChange(e: Event, toggleModalOpen: () => void) {
+  const input = e.target as HTMLInputElement;
+  if (!input.files?.length) return;
+  errorMessage.value = "";
+
+  const file = input.files[0];
+
+  if (!validateFileSize(file.size)) return;
+
+  try {
+    image.value = await readFileAsDataURL(file);
+    if (!(await checkImageDimensions(image.value, props.cropperType))) return;
+    toggleModalOpen();
+  } catch (error: any) {
+    errorMessage.value = `An error occurred while reading the file: ${error.message}`;
+  } finally {
+    // reset the input value
+    if (input) input.value = "";
+  }
+}
 </script>
 
 <style scoped></style>
--- components/VueAdvancedCropper.client.vue
+++ components/VueAdvancedCropper.client.vue
@@ -31,12 +31,29 @@
 
 <script setup lang="ts">
 import type { CropperConfigTypes } from "~/types/cropper";
+import type { CropperResult } from "vue-advanced-cropper";
 import { Cropper, Preview } from "vue-advanced-cropper";
 import "vue-advanced-cropper/dist/style.css";
 
-const cropper = ref(null as typeof Cropper | null);
 
-const p = defineProps({
+//
+const cropper = ref(null as typeof Cropper | null);
+const croppedImage = ref(null as string | null);
+
+const result = reactive({
+  image: null,
+  blob: null,
+  coordinates: {
+    left: 0,
+    top: 0,
+    width: 0,
+    height: 0,
+  },
+});
+
+const config = computed(() => cropperConfigs[props.cropperType]);
+
+const props = defineProps({
   image: {
     type: String,
     required: true,
@@ -47,10 +64,154 @@ const p = defineProps({
   },
 });
 
-const cropType = computed(() => p.cropperType);
-const { result, croppedImage, config, crop, onChange } = useCropper(
-  cropType.value
-);
+// Configs
+export interface CropperConfig {
+  name: string;
+  minHeight: number;
+  minWidth: number;
+  maxHeight?: number;
+  maxWidth?: number;
+  stencilComponent?: string;
+  canvas: {
+    minWidth: number;
+    minHeight: number;
+    maxWidth: number;
+    maxHeight: number;
+  };
+  stencilSize?: {
+    width: number;
+    height: number;
+  };
+  stencilProps: {
+    handlers?: Record<string, any>;
+    movable: boolean;
+    resizable?: boolean;
+    aspectRatio?: number;
+  };
+}
+
+const cropperConfigs: Record<CropperConfigTypes, CropperConfig> = {
+  default: {
+    name: "Default",
+    minWidth: 160,
+    minHeight: 160,
+    canvas: {
+      minHeight: 0,
+      minWidth: 0,
+      maxHeight: 480,
+      maxWidth: 480,
+    },
+    stencilProps: {
+      aspectRatio: 1,
+      movable: true,
+    },
+  },
+  avatar: {
+    name: "avatar",
+    minHeight: 160,
+    minWidth: 160,
+    canvas: {
+      minHeight: 0,
+      minWidth: 0,
+      maxHeight: 480,
+      maxWidth: 480,
+    },
+    stencilProps: {
+      aspectRatio: 1,
+      movable: true,
+    },
+  },
+  cover: {
+    name: "cover",
+    minWidth: 1300,
+    minHeight: 400,
+    canvas: {
+      minHeight: 0,
+      minWidth: 0,
+      maxWidth: 1200,
+      maxHeight: 3900,
+    },
+    stencilProps: {
+      aspectRatio: 3.25,
+      movable: true,
+    },
+  },
+};
+
+
+// Checks & Utils
+type Compression = "lossy" | "lossless" | "alpha" | "animation";
+
+async function checkWebpFeature(feature: Compression): Promise<boolean> {
+  return new Promise((resolve) => {
+    const kTestImages = {
+      lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
+      lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
+      alpha:
+        "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
+      animation:
+        "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA",
+    };
+
+    const img = new Image();
+    img.onload = () => resolve(true);
+    img.onerror = () => resolve(false);
+    img.src = "data:image/webp;base64," + kTestImages[feature];
+  });
+}
+
+function convertImage(canvas: HTMLCanvasElement, mimeType: string) {
+  canvas.toBlob(
+    (blob) => {
+      // load image for testing
+      const newImg = document.createElement("img");
+      const url = URL.createObjectURL(blob);
+      newImg.onload = () => {
+        URL.revokeObjectURL(url); // Clean up
+      };
+      newImg.src = url;
+      document.body.appendChild(newImg);
+      // here we would send to the server
+      console.log("blobUrl", url, blob);
+    },
+    mimeType,
+    1
+  );
+}
+
+// Cropper
+const crop = async (cropper) => {
+  if (!cropper) return;
+
+  const { canvas } = cropper.getResult();
+  if (!canvas) return;
+
+  let webpSupport = false;
+  for (let feature of [
+    "lossy",
+    "lossless",
+    "alpha",
+    "animation",
+  ] as Compression[]) {
+    const result = await checkWebpFeature(feature);
+    if (result) {
+      webpSupport = true;
+      break;
+    }
+  }
+
+  const exportMimeType = webpSupport ? "image/webp" : "image/jpeg";
+
+  convertImage(canvas, exportMimeType);
+};
+
+function onChange({ coordinates, image, canvas }: CropperResult) {
+  // realtime changes in the cropper
+  if (!coordinates || !image) return;
+
+  result.coordinates = coordinates;
+  result.image = image;
+}
 </script>
 
 <style scoped></style>
